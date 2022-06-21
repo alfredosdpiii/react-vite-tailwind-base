bf27f04c00a3890756c2dffeab730c49a3fb5ad3
alfredosdpiii <alfredo.sdpiii@gmail.com>
2022-06-21 20:33:31 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
+    },
+  },
+  plugins: [],
+}
+
